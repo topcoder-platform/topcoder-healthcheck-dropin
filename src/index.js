cb6d46369283dabc900e461257a0c76906f056db
CWD <cwd@lazybaer.com>
2018-07-27 18:54:52 -0400

    added in a callback on start. fixed mod caching issue w/ express. returning a json body for the healthcheck

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 
-const app = express();
 
 const HTTP_HEALTHY = 200;
 const HTTP_FAILED = 503;
@@ -38,7 +37,7 @@ function executeCustomChecks(req, res, checks) {
  */
 function healthCheck(req, res, checks) {
   const result = executeCustomChecks(req, res, checks);
-  res.sendStatus(result ? HTTP_HEALTHY : HTTP_FAILED);
+  res.status((result ? HTTP_HEALTHY : HTTP_FAILED)).json({ checksRun: (checks ? checks.length : 0) });
   return result;
 }
 
@@ -49,7 +48,7 @@ function healthCheck(req, res, checks) {
  * @returns {function}        middleware function
  */
 function middleware(checks) {
-  return (req, res, next) => {
+  return function middle(req, res, next) {
     if (req.url === '/health') {
       healthCheck(req, res, checks);
     } else {
@@ -62,17 +61,33 @@ function middleware(checks) {
  * init - description
  *
  * @param  {array} checks optional array of functions to execute to check service health. functions must return boolean
- * @returns {object}        returns the express app that was initialized
+ * @returns {object}        returns the express server that was initialized
  */
-function init(checks) {
-  app.get('/health', (req, res, next) => next());
-  app.use(middleware(checks));
-  const port = process.env.port || 3000;
-  app.listen(port, () => {
-    console.log(`Topcoder Health Check DropIn listening on port ${port}`);
-  });
+function init(checks, cb) {
+  let checksArray = checks;
+  let svcStartCB = cb;
+  if (typeof checks === 'function') {
+    checksArray = null;
+    svcStartCB = checks;
+  }
 
-  return app;
+  if (!svcStartCB) {
+    svcStartCB = (err) => {
+      if (!err) {
+        console.log('Topcoder Health Check DropIn started and ready to roll');
+      } else {
+        console.log(err);
+      }
+    };
+  }
+
+  const app = express();
+  app.get('/health', middleware(checksArray));
+  const port = process.env.port || 3000;
+  return app.listen(port, (err) => {
+    console.log(`********** Topcoder Health Check DropIn listening on port ${port}`);
+    svcStartCB(err);
+  });
 }
 
 module.exports = {
